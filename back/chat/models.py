612748c6b6af4497db7717b4a6471c10d9e1443d
Alexey Zaharov <alexey.zaharov@lacit.net>
2020-05-17 17:32:16 +0300

    Add dockstrings

--- back/chat/models.py
+++ back/chat/models.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from typing import Optional, List, Union
 
+from motor.motor_asyncio import AsyncIOMotorClient
+
 from profile.models import Profile
 from utils import clean_data
 from utils.mixins import DataMixin
@@ -15,12 +17,18 @@ class Chat(DataMixin):
     messages: Optional[Union[List[dict], List[str]]] = None
 
     async def fetch_messages(self,
-                             db):
+                             db: AsyncIOMotorClient) -> None:
+        """
+        Iterate by messages array, find specific message in db by message_id
+        and replace message_id in array by message document from db
+        Args:
+            db: db client instance.
+
+        """
         for idx, message in enumerate(self.messages):
             _message = await db.messages.find_one(
                 {"message_id": message}
             )
-
             self.messages[idx] = clean_data(_message)
 
 
@@ -33,6 +41,8 @@ class ChatList(DataMixin):
 class Message(DataMixin):
     """This object represents a message."""
     message_id: Optional[int] = None
-    from_user: Optional[Profile] = None
-    date: Optional[int] = None
+    chat_id: Optional[int] = None
+    from_user: Optional[int] = None
+    to_user: Optional[int] = None
+    date: Optional[str] = None
     text: Optional[str] = None
--- back/profile/models.py
+++ back/profile/models.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from typing import Optional, List, Union
 
-from aiohttp import web
+from motor.motor_asyncio import AsyncIOMotorClient
 
 from utils import clean_data
 from utils.mixins import DataMixin
@@ -9,6 +9,7 @@ from utils.mixins import DataMixin
 
 @dataclass()
 class Profile(DataMixin):
+    """Container for user data"""
     user_id: Optional[str] = None
     first_name: Optional[str] = None
     last_name: Optional[str] = None
@@ -18,12 +19,21 @@ class Profile(DataMixin):
     contacts: Optional[Union[List[dict], List[str]]] = None
     chat_id: Optional[str] = None
 
-    def check_password(self, password: str):
+    def check_password(self, password: str) -> bool:
         return self.password == password
 
     async def is_user_exist(self,
-                            db,
-                            request_data):
+                            db: AsyncIOMotorClient,
+                            request_data: dict) -> bool:
+        """
+        Check is user document exist in db.
+        Args:
+            db: db client instance.
+            request_data: dict with user data
+
+        Returns:
+
+        """
         email = await db.users.find_one(
             {"email": request_data['email']})
 
@@ -33,7 +43,14 @@ class Profile(DataMixin):
         return email or username
 
     async def fetch_contacts(self,
-                             db):
+                             db: AsyncIOMotorClient) -> None:
+        """
+        Iterate by contacts array, find specific user in db by user_id
+        and replace user_id in array by user document from db
+        Args:
+            db: db client instance.
+
+        """
         for idx, contact in enumerate(self.contacts):
             _contact = await db.users.find_one(
                 {"user_id": contact}
